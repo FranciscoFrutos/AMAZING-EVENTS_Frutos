64f3b8b5d3b31fc7e6a76cc4be4a2e5ed5301a00
Francisco Frutos <francisco_frutos@outlook.com>
2023-03-22 16:44:14 -0300

    index.js movido a main.js, favicon arreglado, links a redes sociales añadidos; seleccion de filtros y menu sin barras laterales para desktop

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -93,3 +93,15 @@ function filterByCategory(eventsData){
     return filteredList
 }
 
+/* index.html */
+
+async function getData() {
+    const response = await fetch('https://mindhub-xj03.onrender.com/api/amazing');
+    data = await response.json();
+    createCategories(data);
+    addEventFilters(data.events);
+    createCards(data.events);
+    
+  }
+
+getData()
